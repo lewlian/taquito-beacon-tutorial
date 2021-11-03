41f35e38869394344e94d82fb9be924e96be5575
lewlian <seanlewtengsiong@gmail.com>
2021-11-03 16:35:09 +0800

    changed to faucet display

--- index.js
+++ index.js
@@ -1,111 +1,65 @@
-import "babel-polyfill";
 import { BeaconWallet } from "@taquito/beacon-wallet";
 import { Tezos } from "@taquito/taquito";
 
-let userAddress, userBalance, faucetBalance;
-
+const axios = require("axios");
 const faucetAddress = "tz1aaFbFMUhQ7LWXd3o8hQoAGzMuge7HP5n8";
+let userAddress, userBalance, wallet;
+const serverEndpoint =
+	"http://127.0.0.1:3000/getmoney/tz1MAYxo7VL5kFNuun1TKYBzDMsgPtF7PMcE";
 
 window.onload = () => {
 	document.getElementById("init-wallet").onclick = initWallet;
-	document.getElementById("redeem-faucet").onclick = changeMessage;
-	document.getElementById("disconnect").onclick = disconnect;
+	document.getElementById("redeem-faucet").onclick = redeemFaucet;
+	document.getElementById("disconnect").onclick = disconnectWallet;
 };
 
 const updateInnerText = (id, text) =>
 	(document.getElementById(id).innerText = text);
 
-const showToast = (msg) => {
-	const toast = document.getElementById("toast");
-	toast.textContent = msg;
-	setTimeout(() => {
-		toast.className = "show";
-		setTimeout(() => {
-			toast.className = toast.className.replace("show", "");
-		}, 3000);
-	}, 3000);
+const redeemFaucet = async () => {
+	console.log("sending endpoint");
+	axios.get(serverEndpoint).then((resp) => {
+		console.log(resp.data);
+	});
 };
 
 const initWallet = async () => {
 	try {
-		Tezos.setProvider({ rpc: `https://granadanet.api.tez.ie` });
-		// creating new Beacon wallet instance
 		const options = {
-			name: "Taquito Beacon Tutorial",
-			eventHandlers: {
-				PERMISSION_REQUEST_SUCCESS: {
-					handler: async (data) => {
-						console.log("Wallet is connected:", data);
-					},
-				},
-				OPERATION_REQUEST_SENT: {
-					handler: async (data) => {
-						console.log("Request sent:", data);
-					},
-				},
-				OPERATION_REQUEST_SUCCESS: {
-					handler: async (data) => {
-						console.log("Request successful:", data);
-						showToast("Request successful!");
-					},
-				},
-				OPERATION_REQUEST_ERROR: {
-					handler: async (data) => {
-						console.log("Request error:", data);
-						showToast("Request error!");
-					},
-				},
-			},
+			name: "Taquito Wallet",
 		};
-		const wallet = new BeaconWallet(options);
-		// setting up network
+		wallet = new BeaconWallet(options);
 		const network = {
 			type: "granadanet",
+			rpcUrl: "https://rpc.granada.tzstats.com",
 		};
-		// requesting permissions on selected network
+
 		await wallet.requestPermissions({ network });
-		// setting Beacon wallet as wallet provider for Taquito
 		Tezos.setWalletProvider(wallet);
-		// getting user's address
+		//update dapp interface
+		console.log("Your address:", wallet.permissions.address);
 		userAddress = wallet.permissions.address;
 		console.log("Your address:", userAddress);
-		// getting user's balance on Carthagenet
 		userBalance = await Tezos.tz.getBalance(userAddress);
-		faucetBalance = await Tezos.tz.getBalance(faucetAddress);
-		// getting info from smart contract
 
-		// hides button
+		//hides button
 		document.getElementById("connection").style.display = "none";
-		// shows and populates contract interface
+
+		//shows and populates interace
 		document.getElementById("interface").style.display = "block";
 		updateInnerText("user-address", userAddress);
 		updateInnerText("user-balance", userBalance / 1000000);
-		updateInnerText("faucet-balance", faucetBalance / 1000000);
 	} catch (error) {
 		console.log(error);
 	}
 };
 
-const disconnect = () => {
-	wallet.client.destroy();
-	wallet = undefined;
-};
-
-const changeMessage = async () => {
-	// disables confirmation button
-	document.getElementById("redeem-faucet").disabled = true;
-	// displays loader
-	document.getElementById("loader").style.display = "block";
-	const message = document.getElementById("new-message").value;
+const disconnectWallet = async () => {
 	try {
-		const op = await contractInstance.methods.changeMessage(message).send();
-		await op.confirmation();
-		document.getElementById("new-message").value = "";
-		// reloads storage
+		console.log("disconnecting");
+		wallet.client.destroy();
+		wallet = undefined;
 	} catch (error) {
 		console.log(error);
-	} finally {
-		document.getElementById("redeem-faucet").disabled = false;
-		document.getElementById("loader").style.display = "none";
 	}
 };
